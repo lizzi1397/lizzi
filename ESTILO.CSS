0f649fb813b67e52c047315fc2592c37ba322421
lizzi1397 <tuasesora.lizz@gmail.com>
2024-09-30 09:21:11 -0600

    Add files via upload

---
+++ ESTILO.CSS
@@ -0,0 +1,29 @@
+body {background-color: #f2f2f2;
+ font-family: Arial, sans-serif;}
+
+.contenedor {width: 80%;
+ margin: 40px auto;
+ text-align: center;}
+
+.moto {width: 50%;
+ margin: 20px auto;}
+
+.moto img {width: 100%;
+ height: auto;
+ border-radius: 10px;
+ box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);}
+
+.mensaje {width: 50%;
+ margin: 20px auto;
+ background-color: #333;
+ color: #fff;
+ padding: 20px;
+ border-radius: 10px;
+ box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);}
+
+.mensaje h1 {font-size: 24px;
+ font-weight: bold;
+ margin-bottom: 10px;}
+
+.mensaje p {font-size: 18px;
+ margin-bottom: 20px;}
